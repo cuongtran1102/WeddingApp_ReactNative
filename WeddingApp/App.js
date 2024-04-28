37ed68189fe9d5f6409c43efc52e6ac2bb133253
anhquoc23 <nguyenah316@gmail.com>
2024-04-28 11:36:57 +0700

    feat: create usercontext, userreducer, logout

--- WeddingApp/App.js
+++ WeddingApp/App.js
@@ -1,6 +1,6 @@
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
-import React from 'react';
+import React, { useReducer } from 'react';
 import Home from './components/Home/Home';
 import BookingHistory from './components/Booking History/BookingHistory';
 import { Ionicons } from '@expo/vector-icons';
@@ -8,65 +8,89 @@ import CustomDrawer from './components/CustomDrawer/CustomDrawer';
 import EditProfile from './components/EditProfile/EditProfile';
 import ResetPassword from './components/ResetPassword/ResetPassword';
 import Logout from './components/Logout/Logout';
-
+import MyUserReducer from './reducers/MyUserReducers';
+import UserContext from './contexts/UserContext';
+import Login from './components/Login/Login';
 const Drawer = createDrawerNavigator();
 
 
 export default function App() {
+  const [currentUser, dispatch] = useReducer(MyUserReducer, null)
+
+
   return (
-    <NavigationContainer>
-      <Drawer.Navigator
-        drawerContent={(props) => <CustomDrawer {...props}
-        />}
-        initialRouteName='Home' screenOptions={{
-          headerTitleAlign: 'center',
-          headerStyle: {
-            backgroundColor: '#98fb98',
-          },
-          headerTintColor: '#f0ffff',
-        }}>
-        <Drawer.Screen
-          name='Home'
-          component={Home}
-          options={{
-            title: 'Dịch Vụ Đặt Tiệc', drawerIcon: ({ focused, color, size }) => (
-              <Ionicons name={focused ? 'home' : 'home-outline'} size={size} color={color} />
-            ),
-          }} />
-        <Drawer.Screen
-          name='BookingHistory'
-          component={BookingHistory}
-          options={{
-            title: 'Lịch Sử Đặt Tiệc', drawerIcon: ({ focused, color, size }) => (
-              <Ionicons name={focused ? 'time' : 'time-outline'} size={size} color={color} />
-            )
-          }} />
+    <UserContext.Provider value={[currentUser, dispatch]}>
+      <NavigationContainer>
+        <Drawer.Navigator
+          drawerContent={(props) => <CustomDrawer {...props}
+          />}
+          initialRouteName='Login' screenOptions={{
+            headerTitleAlign: 'center',
+            headerStyle: {
+              backgroundColor: '#98fb98',
+            },
+            headerTintColor: '#f0ffff',
+          }}>
+            <Drawer.Screen
+              name='Home'
+              component={Home}
+              options={{
+                title: 'Dịch Vụ Đặt Tiệc', drawerIcon: ({ focused, color, size }) => (
+                  <Ionicons name={focused ? 'home' : 'home-outline'} size={size} color={color} />
+                ),
+              }} />
+          
+          
           <Drawer.Screen
-          name='EditProfile'
-          component={EditProfile}
-          options={{
-            title: 'Chỉnh sửa thông tin', drawerIcon: ({ focused, color, size }) => (
-              <Ionicons name={focused ? 'settings' : 'settings-outline'} size={size} color={color} />
-            )
-          }} />
+            name='BookingHistory'
+            component={BookingHistory}
+            options={{
+              title: 'Lịch Sử Đặt Tiệc', drawerIcon: ({ focused, color, size }) => (
+                <Ionicons name={focused ? 'time' : 'time-outline'} size={size} color={color} />
+              )
+            }} />
+
+            { currentUser !== null ? 
+            
+              <>
+                <Drawer.Screen
+            name='EditProfile'
+            component={EditProfile}
+            options={{
+              title: 'Chỉnh sửa thông tin', drawerIcon: ({ focused, color, size }) => (
+                <Ionicons name={focused ? 'settings' : 'settings-outline'} size={size} color={color} />
+              )
+            }} />
           <Drawer.Screen
-          name='ResetPassword'
-          component={ResetPassword}
-          options={{
-            title: 'Đổi mật khẩu', drawerIcon: ({ focused, color, size }) => (
-              <Ionicons name={focused ? 'lock-closed' : 'lock-closed-outline'} size={size} color={color} />
-            )
-          }} />
+            name='ResetPassword'
+            component={ResetPassword}
+            options={{
+              title: 'Đổi mật khẩu', drawerIcon: ({ focused, color, size }) => (
+                <Ionicons name={focused ? 'lock-closed' : 'lock-closed-outline'} size={size} color={color} />
+              )
+            }} />
           <Drawer.Screen
-          name='Logout'
-          component={Logout}
-          options={{
-            title: 'Đăng xuất', drawerIcon: ({ focused, color, size }) => (
-              <Ionicons name={focused ? 'log-out' : 'log-out-outline'} size={size} color={color} />
-            )
-          }} />
-      </Drawer.Navigator>
-    </NavigationContainer>
+            name='Logout'
+            component={Logout}
+            options={{
+              title: 'Đăng xuất', drawerIcon: ({ focused, color, size }) => (
+                <Ionicons name={focused ? 'log-out' : 'log-out-outline'} size={size} color={color} />
+              )
+            }} />
+              </> : <Drawer.Screen
+            name='Login'
+            component={Login}
+            options={{
+              title: 'Đăng Nhập', drawerIcon: ({ focused, color, size }) => (
+                <Ionicons name={focused ? 'home' : 'home-outline'} size={size} color={color} />
+              ),
+            }} />
+              
+          }
+          
+        </Drawer.Navigator>
+      </NavigationContainer>
+    </UserContext.Provider>
   );
 }
 
--- WeddingApp/configs/API.js
+++ WeddingApp/configs/API.js
@@ -3,7 +3,8 @@ import axios from "axios"
 const SERVER = 'https://anhquoc0304.pythonanywhere.com'
 export const Endpoints = {
     'user': {
-        'login': '/o/token/'
+        'login': '/o/token/',
+        'current': '/users/current-user/'
     }
 }
 
